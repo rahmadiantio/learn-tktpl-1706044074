59b38e6fde69bac0411f70eb735b902f260ca803
rahmadiantio <t.rahmadian@gmail.com>
2020-10-12 00:21:43 +0700

    feat: implementation fragment in List Biology's Kingdom

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/RahmadianTioPratama/helloworld/MainActivity.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/RahmadianTioPratama/helloworld/MainActivity.java
@@ -1,16 +1,34 @@
 package id.ac.ui.cs.mobileprogramming.RahmadianTioPratama.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-import id.ac.ui.cs.mobileprogramming.RahmadianTioPratama.helloworld.R;
+import androidx.fragment.app.FragmentTransaction;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements NamaKingdomFragment.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            NamaKingdomFragment namaKingdomFragment = new NamaKingdomFragment();
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.add(R.id.flContainer, namaKingdomFragment);
+            ft.commit();
+        }
+    }
+
+    @Override
+    public void onKingdomSelected(int position) {
+        DeskripsiKingdomFragment deskripsiKingdomFragment = new DeskripsiKingdomFragment();
+        Bundle args = new Bundle();
+        args.putInt("position", position);
+        deskripsiKingdomFragment.setArguments(args);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.flContainer, deskripsiKingdomFragment)
+                .addToBackStack(null)
+                .commit();
     }
 }
