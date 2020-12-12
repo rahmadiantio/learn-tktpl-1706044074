f9797aeb48820e3775a3fa27f5530bde17feccd6
rahmadiantio <t.rahmadian@gmail.com>
2020-12-13 01:38:46 +0700

    project flutter in native android

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/RahmadianTioPratama/helloworld/MainActivity.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/RahmadianTioPratama/helloworld/MainActivity.java
@@ -3,14 +3,30 @@ package id.ac.ui.cs.mobileprogramming.RahmadianTioPratama.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import id.ac.ui.cs.mobileprogramming.RahmadianTioPratama.helloworld.R;
 
 public class MainActivity extends AppCompatActivity {
+    int hitung = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TextView tv = findViewById(R.id.counter);
+        FloatingActionButton btnTambah = findViewById(R.id.btnTambah);
+
+        btnTambah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv.setText(Integer.toString(hitung++));
+            }
+        });
     }
 }
